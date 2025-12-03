5119347ec7a8344945557d7c930d77ea808aa95b
SunthornPuu <phakinphakin9@gmail.com>
2025-12-03 13:22:29 +0700

    hero_added

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+
+export default function Hero() {
+
+    const videoRef = useRef();
+    useEffect(() => {
+        videoRef.current.playbackRate = 2;
+    })
+
+    return (
+        <section className="m-20 flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold text-white">MacBook Pro</h1>
+            <img className="w-1/2" src="/title.png" alt="title" />
+            <video ref={videoRef} src="/videos/hero.mp4" autoPlay muted />
+            <button className="rounded-4xl bg-blue-500 w-20 h-10 text-white text-xl
+            ">Buy</button>
+            <br />
+            <p className="text-xl">From $1599 ot $$133/mo for the first 12 months</p>
+        </section>
+    );
+}
